2edb3f8604fba7f0302c35935bf2ebf4a2d1a8ee
nicosammito <nsammito@code0.tech>
2026-07-06 19:50:23 +0200

    feat: make parameters optional in CustomContent schema and function signature

--- actions/gls-action/src/data_types/glsCustomContent.ts
+++ actions/gls-action/src/data_types/glsCustomContent.ts
@@ -2,8 +2,8 @@ import { DisplayMessage, Identifier, Name, Schema } from "@code0-tech/hercules";
 import { z } from "zod";
 
 export const CustomContentSchema = z.object({
-    CustomerLogo: z.string(),
-    BarcodeContentType: z.enum(["TRACK_ID", "GLS_SHIPMENT_REFERENCE"]),
+    CustomerLogo: z.string().nullish(),
+    BarcodeContentType: z.enum(["TRACK_ID", "GLS_SHIPMENT_REFERENCE"]).nullish(),
     Barcode: z.string().nullish(),
     BarcodeType: z.enum(["EAN_128", "CODE_39"]).nullish(),
     HideShipperAddress: z.boolean().nullish(),
